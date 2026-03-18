2b8a76284712bc839bca30bcb45f94a011ba34c4
zipweb <thiago1010@msn.com>
2026-03-17 22:18:22 -0300

    Update page.tsx

--- apps/frontend/src/app/reset-password/page.tsx
+++ apps/frontend/src/app/reset-password/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import '../../components/lms/lms.css';
